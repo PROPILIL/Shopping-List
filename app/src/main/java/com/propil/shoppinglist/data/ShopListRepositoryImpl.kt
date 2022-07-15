da832f7eccb05558239a41f8e0c91ae9fd92438b
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-07-15 13:46:11 +0300

    Data layer created

---
+++ app/src/main/java/com/propil/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.propil.shoppinglist.data
+
+import com.propil.shoppinglist.domain.ShopItem
+import com.propil.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl: ShopListRepository {
+    override fun getShopList(): List<ShopItem> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+}
