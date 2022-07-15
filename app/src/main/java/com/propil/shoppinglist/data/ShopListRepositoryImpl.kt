4c17da3554b5c5760eb9927b0782ee9408019c34
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-07-15 14:09:09 +0300

    Very simple Data repository realisation

--- app/src/main/java/com/propil/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/propil/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -2,25 +2,38 @@ package com.propil.shoppinglist.data
 
 import com.propil.shoppinglist.domain.ShopItem
 import com.propil.shoppinglist.domain.ShopListRepository
+import java.lang.RuntimeException
 
 object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementedId = 0
+
     override fun getShopList(): List<ShopItem> {
-        TODO("Not yet implemented")
+        return shopList.toList()
     }
 
     override fun addShopItem(shopItem: ShopItem) {
-        TODO("Not yet implemented")
+        if (shopItem.id == -1) {
+            shopItem.id = autoIncrementedId++
+        }
+        shopList.add(shopItem)
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
-        TODO("Not yet implemented")
+        shopList.remove(shopItem)
     }
 
     override fun getShopItem(shopItemId: Int): ShopItem {
-        TODO("Not yet implemented")
+        return shopList.find {
+            it.id == shopItemId
+            } ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
     override fun editShopItem(shopItem: ShopItem) {
-        TODO("Not yet implemented")
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+
     }
 }
