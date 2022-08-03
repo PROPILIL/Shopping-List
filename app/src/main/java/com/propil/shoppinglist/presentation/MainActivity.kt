ca9d63a9973b03f751bc6ae033aecdc627abaf00
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-08-03 15:25:19 +0300

    - RecyclerView optimized - Constants added - ViewType switch added

--- app/src/main/java/com/propil/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/propil/shoppinglist/presentation/MainActivity.kt
@@ -10,7 +10,7 @@ import com.propil.shoppinglist.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: ShopListAdapter
+    private lateinit var shopListAdapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +20,15 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopListLiveData.observe(this) {
-            adapter.shopList = it
+            shopListAdapter.shopList = it
         }
     }
 
     private fun setupRecyclerView() {
         val recyclerViewShopList = findViewById<RecyclerView>(R.id.main_recycler)
-        adapter = ShopListAdapter()
-        adapter = adapter
         with (recyclerViewShopList) {
+            shopListAdapter = ShopListAdapter()
+            adapter = shopListAdapter
             recycledViewPool.setMaxRecycledViews(
                 ShopListAdapter.VIEW_TYPE_ENABLED,
                 ShopListAdapter.MAX_VIEW_HOLDER_POOL
